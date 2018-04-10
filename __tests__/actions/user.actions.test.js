b6c1f1e1bb25f0e2e958dbd3dc6ad2e014c7ce07
Adam Klein <adam@500tech.com>
2018-04-10 19:01:11 +0300

    Removed user actions

--- __tests__/actions/user.actions.test.js
+++
@@ -1,7 +0,0 @@
-import { setUserToken } from 'actions/user.actions';
-
-describe('Actions: user', () => {
-  test('setUserToken', () => {
-    expect(setUserToken('token')).toMatchSnapshot();
-  });
-});

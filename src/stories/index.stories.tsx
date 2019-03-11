c3f9abcc2021a063a31e0fff182372ae31b89f91
Maayan Glikser <maayan@glikm.com>
2019-03-11 11:08:14 +0200

    supress storybook warning for typings

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+// @ts-ignore
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);

62ad129ecdfebbc19612802e5b7f581cf2163b31
Maayan Glikser <maayan@glikm.com>
2019-10-16 14:24:07 +0300

    test lint staged

--- src/sample/lazy.tsx
+++ src/sample/lazy.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Lazy = () => <div>Lazy Loaded</div>;
+const Lazy = () => (
+  <div data-a={2} data-b={2} data-c={2} data-d={2} data-e={2}>
+    Lazy Loaded
+  </div>
+);
 
 export default Lazy;

a602c969fa2111dbc76fbfe6e86464513e33f0fe
Maayan Glikser <maayan@glikm.com>
2019-10-16 16:09:09 +0300

    Upgrade to new storybook setup

---
+++ .storybook/config.js
@@ -0,0 +1,24 @@
+import { configure, addDecorator } from '@storybook/react';
+
+// TODO: Remove me if no need for localization
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
+
+import locales from '../src/constants/locales';
+import moment from 'moment';
+import forEach from 'lodash/forEach';
+
+forEach(locales, (locale, key) =>
+  moment.defineLocale(key, locale.dateTimeFormat)
+);
+
+setIntlConfig({
+  locales: Object.keys(locales),
+  defaultLocale: 'en-US',
+  getMessages: locale => locales[locale].translations
+});
+
+addDecorator(withIntl);
+// END LOCALIZATION TODO
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../stories', true, /\.stories\.js$/), module);
--- stories/sample/Post.stories.js
+++ stories/sample/Post.stories.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
-import { StyledPost } from 'sample/sample';
+import { StyledPost } from '../../src/sample/sample';
+import { FormattedMessage } from 'react-intl';
 
 const stories = storiesOf('Sample', module);
 stories.addDecorator(withKnobs);
 
 stories.add('Post', () => (
   <StyledPost>
+    <FormattedMessage id="sample.homepage.title" />
     <h4>{text('Title', 'Post Title')}</h4>
     <p>{text('Description', 'This is the description')}</p>
   </StyledPost>

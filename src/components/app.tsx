7693b4429f1b33132a34652dd095b8d6ee6c9629
Maayan Glikser <maayan@glikm.com>
2021-01-31 18:15:47 +0200

    Comment out optional features in app and connect history api to the router

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,18 @@
 import React, { lazy, Suspense } from 'react';
 import { ThemeProvider } from '@emotion/react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router';
 import { Provider } from 'react-redux';
 
 import store from 'store';
-import { theme } from 'constants/themes.constants';
+import history from 'utils/history';
+// import { theme } from 'constants/themes.constants';
 
 import Localization from 'components/localization'; // TODO: remove if no localization
 import Layout from 'components/layout/layout';
 import Loading from 'sample/loading';
-import Sample from 'sample/sample';
-import { GlobalStyles } from 'components/global-styles'; // TODO: replace this with actual component
+import Sample from 'sample/sample'; // TODO: replace this with actual component
+import { GlobalStyles } from 'components/global-styles';
 
 const Lazy = lazy(() => import('sample/lazy'));
 
@@ -19,19 +20,19 @@ function App() {
   return (
     <Provider store={store}>
       <Localization>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <BrowserRouter>
-            <Layout>
-              <Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" name="sample" component={Sample} />
-                  <Route path="/lazy" name="lazy" component={Lazy} />
-                </Switch>
-              </Suspense>
-            </Layout>
-          </BrowserRouter>
-        </ThemeProvider>
+        {/*<ThemeProvider theme={theme}>*/}
+        <GlobalStyles />
+        <Router history={history}>
+          <Layout>
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" name="sample" component={Sample} />
+                <Route path="/lazy" name="lazy" component={Lazy} />
+              </Switch>
+            </Suspense>
+          </Layout>
+        </Router>
+        {/*</ThemeProvider>*/}
       </Localization>
     </Provider>
   );

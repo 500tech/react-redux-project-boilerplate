dd90ca915fdb9d2cf058703eadfe4c747a517e6d
Maayan Glikser <maayan@glikm.com>
2021-01-31 18:12:23 +0200

    Improve typings in store

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware, StoreEnhancer } from 'redux';
 import { RESTORE_LOCAL_STORAGE_KEY } from 'constants/restore.constants';
 
 import rootReducer from 'reducers/root';
@@ -7,7 +7,7 @@ import apiMiddleware from 'middlewares/api';
 const isDev = process.env.NODE_ENV !== 'production';
 
 declare const window: Window & {
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?(a: any): void;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?(a: any): StoreEnhancer;
 };
 
 const composeEnhancers =
@@ -23,7 +23,7 @@ const savedState = localStorage.getItem(RESTORE_LOCAL_STORAGE_KEY);
 
 const store = createStore(
   rootReducer,
-  ...(savedState && isDev ? [JSON.parse(savedState)] : []),
+  savedState && isDev ? JSON.parse(savedState) : undefined,
   // @ts-ignore
   composeEnhancers(applyMiddleware(...middlewares))
 );

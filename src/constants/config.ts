0378a29ad1e8fc65519e16c7b726cc79ef5e2603
Maayan Glikser <maayan@glikm.com>
2021-01-31 18:13:59 +0200

    remove type stub for process as it's no longer needed

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,2 @@
-declare const process: {
-  env: {
-    [key: string]: string;
-  };
-};
-
-export const BASE_URL: string = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const IS_DEBUG_MODE: boolean = Boolean(process.env.DEBUG);

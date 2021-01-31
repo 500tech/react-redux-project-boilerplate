328ee5b900737f50bc11e36fc07eaf17714d5125
Maayan Glikser <maayan@glikm.com>
2021-01-31 18:13:37 +0200

    remove notifications type hint and update typings for meta to Record

--- src/types/base-redux.ts
+++ src/types/base-redux.ts
@@ -1,14 +1,5 @@
 export interface BaseAction {
   type: string;
-  meta?: {
-    [key: string]: any;
-    notifications?: {
-      // TODO: remove this if not needed (not implemented, just a definition)
-      success?: {
-        title?: string;
-        values?: object;
-      };
-    };
-  };
+  meta?: Record<string, any>;
   payload?: any;
 }

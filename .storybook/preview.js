78c996f1d818ef22f7ee6a6a4fed0a1ae1dbf1a2
Maayan Glikser <maayan@glikm.com>
2021-01-31 18:12:53 +0200

    remove moment from storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,7 @@
-import { addDecorator } from '@storybook/react';
-
 // TODO: Remove me if no need for localization
 import { setIntlConfig, withIntl } from 'storybook-addon-intl';
 
 import locales from '../src/constants/locales';
-import moment from 'moment';
-import forEach from 'lodash/forEach';
-
-forEach(locales, (locale, key) =>
-  moment.defineLocale(key, locale.dateTimeFormat)
-);
 
 setIntlConfig({
   locales: Object.keys(locales),
